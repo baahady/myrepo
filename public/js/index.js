4b96ffc41097ad230e65de5773b985902ae1cbe8
Bahram <ba.ahady@gmail.com>
2019-04-19 17:34:08 +0430

    add form and show messages with jQuery

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,19 @@
 
 	socket.on('newMessage',function(message){
 		console.log('newMessage',message);
+		var li = jQuery('<li></li>');
+		li.text(`${message.from}: ${message.text}`);
+		jQuery('#messages').append(li);
 	});
 
-	socket.emit('createMessage',{
-		from:'bahram',
-		text: 'Im Alive'
-	},function(data){
-		console.log('got it: ',data);
+	jQuery('#message-form').on('submit',function(e){
+		e.preventDefault();
+
+		socket.emit('createMessage',{
+			from: 'User',
+			text: jQuery('[name=message]').val()
+		},function(){
+
+		});
 	});
+
