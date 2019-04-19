0328e0f50fbbf4c96eca53184d5f5a639ea170ee
Bahram <ba.ahady@gmail.com>
2019-04-19 11:05:17 +0430

    messageGenerator function

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicpath = path.join(__dirname,'../public');
 
 const port = process.env.PORT || 3000;
@@ -32,11 +33,7 @@ io.on('connection',(socket)=>{
 
 	socket.on('createMessage',(message)=>{
 		console.log('createMessage',message);
-		io.emit('newMessage',{
-			from: message.from,
-			text:message.text,
-			createdAt: new Date().getTime()
-		});
+		io.emit('newMessage',generateMessage(message.from,message.text));
 		// socket.broadcast.emit('newMessage',{
 		// 	from: message.from,
 		// 	text: message.text,
